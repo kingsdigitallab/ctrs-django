035e9de0a68a97db3c1bc980b135fe40046f024a
Ginestra Ferraro <ginestra@users.noreply.github.com>
2018-05-03 14:31:28 +0000

    Added js-cookie and implemented it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
-$(function() {
-    // Your JS Here
+$(document).ready(function() {
+    if (!Cookies.get('ctrs-cookie')) {
+        $("#cookie-disclaimer").removeClass('hide');
+    }
+    // Set cookie and hide the box
+    $('#cookie-disclaimer .close').on("click", function() {
+        Cookies.set('ctrs-cookie', 'ctrs-cookie-set', { expires: 30 });
+        $("#cookie-disclaimer").addClass('hide');
+    });
 });
