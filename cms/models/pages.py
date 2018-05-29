59bb932bff9f419feccadd6cd612193f5f08e7f4
Ginestra Ferraro <ginestra@users.noreply.github.com>
2018-05-29 14:59:01 +0000

    Added Blog page types

--- cms/models/pages.py
+++ cms/models/pages.py
@@ -4,10 +4,17 @@ import logging
 
 from django.conf import settings
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db import models
+from django.shortcuts import render
+from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.contrib.routable_page.models import route
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
-from .behaviours import WithStreamField
+from .behaviours import WithFeedImage, WithStreamField
 
 logger = logging.getLogger(__name__)
 
@@ -29,6 +36,7 @@ def _paginate(request, items):
 
 class HomePage(Page, WithStreamField):
     subpage_types = [
+        'BlogIndexPage',
         'IndexPage',
         'RichTextPage'
     ]
@@ -54,6 +62,68 @@ IndexPage.content_panels = [
 IndexPage.promote_panels = Page.promote_panels
 
 
+class BlogIndexPage(Page, WithStreamField):
+    subpage_types = ['BlogPost']
+
+    @property
+    def posts(self):
+        # gets list of live blog posts that are descendants of this page
+        posts = BlogPost.objects.live().descendant_of(self)
+
+        # orders by most recent date first
+        posts = posts.order_by('-date')
+
+        return posts
+
+    @route(r'^$')
+    def all_posts(self, request):
+        posts = self.posts
+        logger.debug('Posts: {}'.format(posts))
+
+        return render(request, self.get_template(request),
+                      {'self': self, 'posts': _paginate(request, posts)})
+
+    @route(r'^author/(?P<author>[\w ]+)/$')
+    def author(self, request, author=None):
+        if not author:
+            # Invalid author filter
+            logger.error('Invalid author filter')
+            return self.all_posts(request)
+
+        posts = self.posts.filter(owner__username=author)
+
+        return render(
+            request, self.get_template(request), {
+                'self': self, 'posts': _paginate(request, posts),
+                'filter_type': 'author', 'filter': author
+            }
+        )
+
+    @route(r'^tag/(?P<tag>[\w\- ]+)/$')
+    def tag(self, request, tag=None):
+        if not tag:
+            # Invalid tag filter
+            logger.error('Invalid tag filter')
+            return self.all_posts(request)
+
+        posts = self.posts.filter(tags__name=tag)
+
+        return render(
+            request, self.get_template(request), {
+                'self': self, 'posts': _paginate(request, posts),
+                'filter_type': 'tag', 'filter': tag
+            }
+        )
+
+
+BlogIndexPage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    StreamFieldPanel('body'),
+]
+
+BlogIndexPage.promote_panels = Page.promote_panels
+
+
 class RichTextPage(Page, WithStreamField):
     subpage_types = []
 
@@ -64,3 +134,31 @@ RichTextPage.content_panels = [
 ]
 
 RichTextPage.promote_panels = Page.promote_panels
+
+
+class BlogPostTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPost', related_name='tagged_items')
+
+
+class BlogPost(Page, WithFeedImage, WithStreamField):
+    tags = ClusterTaggableManager(through=BlogPostTag, blank=True)
+    date = models.DateField('Post date')
+
+    subpage_types = []
+
+    @property
+    def blog_index(self):
+        # finds closest ancestor which is a blog index
+        return self.get_ancestors().type(BlogIndexPage).last()
+
+
+BlogPost.content_panels = [
+    FieldPanel('title', classname='full title'),
+    FieldPanel('date'),
+    StreamFieldPanel('body')
+]
+
+BlogPost.promote_panels = Page.promote_panels + [
+    ImageChooserPanel('feed_image'),
+    FieldPanel('tags')
+]
