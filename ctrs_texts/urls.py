c8d2f6892dacc922d7fe9e577073f5873771b34f
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-01-20 15:13:35 +0000

    AC-74: Added web API views and routers. Renamed old text viewier as legacy viewer.

--- ctrs_texts/urls.py
+++ ctrs_texts/urls.py
@@ -1,12 +1,33 @@
 # from django.conf import settings
-from django.urls import path, re_path
+from django.urls import path
 from django.contrib import admin
-from .views import view_texts, view_text_viewer
+from .views.texts_html import (
+    view_texts,
+    view_text_viewer, view_text_viewer_legacy,
+)
+from .views.texts_json import view_api_texts, view_api_text_chunk
 
 admin.autodiscover()
 
 urlpatterns = [
-    path(r'texts/', view_texts, name="texts"),
-    re_path(r'texts/(?P<text_slug>[^/]+)/(?P<view_slug>[^/]+)/',
-            view_text_viewer, name='text_view'),
+    # List of all texts
+    path('texts/', view_texts, name="texts"),
+
+    # New Text Viewer (multi-texts and multi-views)
+    path('texts/viewer/',
+         view_text_viewer, name='text_viewer'),
+
+    # Web API
+    path(
+        'api/texts/',
+        view_api_texts, name='view_api_texts'
+    ),
+    path(
+        'api/texts/<str:text_slug>/<slug:view>/<slug:unit>/<slug:location>/',
+        view_api_text_chunk, name='view_api_text_chunk'
+    ),
+
+    # Legacy Text Viewer (only one text at a time)
+    path('texts/<slug:text_slug>/<slug:view>/',
+         view_text_viewer_legacy, name='text_viewer_legacy'),
 ]
